ac51ca92480d441e7a51f8ac8cb6dd7c96ff293d
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-10-29 08:51:30 +0200

    Created FitnessApp.Wep Project

---
+++ CSharpWeb-FitnessApp-Sept2024/FitnessApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessApp.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
