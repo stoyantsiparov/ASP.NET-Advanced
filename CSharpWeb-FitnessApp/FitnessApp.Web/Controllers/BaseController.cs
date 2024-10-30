0a41b746df065aa8f7f48401735301594b452907
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-10-30 14:19:59 +0200

    Deleted the first project and created new
    I did it because the identity scaffolding was giving error

---
+++ CSharpWeb-FitnessApp/FitnessApp.Web/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp.Web.Controllers
+{
+	[Authorize]
+	public class BaseController : Controller
+	{
+		protected string GetUserId()
+		{
+			string id = string.Empty;
+
+			if (User != null)
+			{
+				id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			}
+			return id;
+		}
+	}
+}
---
+++ CSharpWeb-FitnessApp/FitnessApp.Web/Controllers/SpaProcedureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp.Web.Controllers
+{
+	public class SpaProcedureController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
