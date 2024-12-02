59f40e0444a89a400da9758c0ba567a9e95fa671
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-02 15:45:19 +0200

    Add ViewModels for adding and deleting fitness events

---
+++ CSharpWeb-FitnessApp/FitnessApp.Web.ViewModels/FitnessEventViewModels/AddFitnessEventViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessApp.Web.ViewModels.FitnessEventViewModels;
+using static FitnessApp.Common.EntityValidationConstants.FitnessEvent;
+
+public class AddFitnessEventViewModel
+{
+    [Required]
+    [MaxLength(TitleMaxLength)]
+    [MinLength(TitleMinLength)]
+    public string Title { get; set; } = null!;
+
+    public string? ImageUrl { get; set; }
+
+    [Required]
+    [MaxLength(DescriptionMaxLength)]
+    [MinLength(DescriptionMinLength)]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    [MaxLength(LocationMaxLength)]
+    [MinLength(LocationMinLength)]
+    public string Location { get; set; } = null!;
+
+    [Required]
+    public string StartDate { get; set; } = null!;
+
+    [Required]
+    public string EndDate { get; set; } = null!;
+}
