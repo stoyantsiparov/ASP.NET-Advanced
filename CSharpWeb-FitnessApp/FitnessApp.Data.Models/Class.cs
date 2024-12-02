c1a71acd2fe0c8d5ba3b74d184c6df0b58031dc6
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-02 20:48:21 +0200

    Add Price property to Class model and reformat code

--- CSharpWeb-FitnessApp/FitnessApp.Data.Models/Class.cs
+++ CSharpWeb-FitnessApp/FitnessApp.Data.Models/Class.cs
@@ -7,39 +7,44 @@ namespace FitnessApp.Data.Models;
 
 public class Class
 {
-    [Comment("Primary key")]
-    [Key]
-    public int Id { get; set; }
+	[Comment("Primary key")]
+	[Key]
+	public int Id { get; set; }
 
-    [Required]
-    [Comment("Name of the fitness class")]
-    [MaxLength(NameMaxLength)]
-    public string Name { get; set; } = null!;
+	[Required]
+	[Comment("Name of the fitness class")]
+	[MaxLength(NameMaxLength)]
+	public string Name { get; set; } = null!;
 
-    [Comment("Image URL of the fitness class")]
-    public string? ImageUrl { get; set; }
+	[Comment("Image URL of the fitness class")]
+	public string? ImageUrl { get; set; }
 
-    [Required]
-    [Comment("Description of the fitness class")]
-    [MaxLength(DescriptionMaxLength)]
-    public string Description { get; set; } = null!;
+	[Required]
+	[Comment("Description of the fitness class")]
+	[MaxLength(DescriptionMaxLength)]
+	public string Description { get; set; } = null!;
 
-    [Required]
-    [Comment("Date and time of the fitness class")]
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = ScheduleDateTimeFormat, ApplyFormatInEditMode = true)]
-    public DateTime Schedule { get; set; }
+	[Required]
+	[Comment("Price for the fitness class")]
+	[Range((double)PriceMinValue, (double)PriceMaxValue)]
+	public decimal Price { get; set; }
 
-    [Required]
-    [Comment("Duration of the fitness class in minutes")]
-    [Range(DurationMinValue, DurationMaxValue)]
-    public int Duration { get; set; }
+	[Required]
+	[Comment("Date and time of the fitness class")]
+	[DataType(DataType.Date)]
+	[DisplayFormat(DataFormatString = ScheduleDateTimeFormat, ApplyFormatInEditMode = true)]
+	public DateTime Schedule { get; set; }
 
-    [Comment("Instructor of the fitness class")]
-    [ForeignKey(nameof(Instructor))]
-    public int InstructorId { get; set; }
-    public Instructor Instructor { get; set; } = null!;
+	[Required]
+	[Comment("Duration of the fitness class in minutes")]
+	[Range(DurationMinValue, DurationMaxValue)]
+	public int Duration { get; set; }
 
-    [Comment("Members registered for the fitness class")]
-    public virtual ICollection<ClassRegistration> ClassesRegistrations { get; set; } = new List<ClassRegistration>();
+	[Comment("Instructor of the fitness class")]
+	[ForeignKey(nameof(Instructor))]
+	public int InstructorId { get; set; }
+	public Instructor Instructor { get; set; } = null!;
+
+	[Comment("Members registered for the fitness class")]
+	public virtual ICollection<ClassRegistration> ClassesRegistrations { get; set; } = new List<ClassRegistration>();
 }
