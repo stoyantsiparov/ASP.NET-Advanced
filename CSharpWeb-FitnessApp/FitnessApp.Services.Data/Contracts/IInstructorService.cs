2f25a6a23af1f2cdee8dc7c710500c75eb825f74
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-03 21:23:27 +0200

    Add IInstructorService interface for managing instructors

---
+++ CSharpWeb-FitnessApp/FitnessApp.Services.Data/Contracts/IInstructorService.cs
@@ -0,0 +1,15 @@
+﻿using FitnessApp.Web.ViewModels.InstructorViewModels;
+
+namespace FitnessApp.Services.Data.Contracts;
+
+public interface IInstructorService
+{
+    Task<IEnumerable<AllInstructorsViewModel>> GetAllInstructorsAsync();
+    Task<InstructorViewModel?> GetInstructorByIdAsync(int id);
+    Task<InstructorDetailsViewModel?> GetInstructorDetailsAsync(int id);
+    Task<AddInstructorViewModel> GetInstructorForAddAsync();
+    Task AddInstructorAsync(AddInstructorViewModel model, string userId);
+    Task EditInstructorAsync(InstructorViewModel model);
+    Task<DeleteInstructorViewModel?> GetInstructorForDeleteAsync(int id);
+    Task DeleteInstructorAsync(int id);
+}
