c9187eee57a8ed69396cc70c1ced8d5e8661f75c
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-03 13:26:57 +0200

    Add ViewModel classes for adding and deleting memberships

---
+++ CSharpWeb-FitnessApp/FitnessApp.Web.ViewModels/MembershipTypeViewModels/AddMembershipTypeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessApp.Web.ViewModels.MembershipTypeViewModels;
+using static FitnessApp.Common.EntityValidationConstants.MembershipType;
+
+public class AddMembershipTypeViewModel
+{
+	[Required]
+	[MaxLength(NameMaxLength)]
+	[MinLength(NameMinLength)]
+	public string Name { get; set; } = null!;
+
+	[Required]
+	[Range((double)PriceMinValue, (double)PriceMaxValue)]
+	public decimal Price { get; set; }
+
+	[Required]
+	[Range(DurationMinValue, DurationMaxValue)]
+	public int Duration { get; set; }
+
+	[Required]
+	[MaxLength(DescriptionMaxLength)]
+	[MinLength(DescriptionMinLength)]
+	public string Description { get; set; } = null!;
+	public string? ImageUrl { get; set; }
+}
---
+++ CSharpWeb-FitnessApp/FitnessApp.Web.ViewModels/MembershipTypeViewModels/DeleteMembershipTypeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FitnessApp.Web.ViewModels.MembershipTypeViewModels;
+
+public class DeleteMembershipTypeViewModel
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = null!;
+	public string Description { get; set; } = null!;
+}
