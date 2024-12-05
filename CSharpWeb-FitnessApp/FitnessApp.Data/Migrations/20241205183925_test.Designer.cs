52cd58a88f429ddd2cd8a5d9b3b7143552fc4489
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-05 20:57:29 +0200

    Adding additional migrations

--- CSharpWeb-FitnessApp/FitnessApp.Data/Migrations/20241205183925_test.Designer.cs
+++ CSharpWeb-FitnessApp/FitnessApp.Data/Migrations/20241205183925_test.Designer.cs
@@ -4,6 +4,7 @@ using FitnessApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FitnessApp.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241205183925_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
