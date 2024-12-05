11792a8743849c3f76f347870d7f0d2e0bf06504
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-06 00:23:12 +0200

    Enhance identity config and add Razor Pages support

--- CSharpWeb-FitnessApp/FitnessApp.Web/Program.cs
+++ CSharpWeb-FitnessApp/FitnessApp.Web/Program.cs
@@ -1,4 +1,5 @@
 using FitnessApp.Data;
+using FitnessApp.Data.Configuration;
 using FitnessApp.Services.Data;
 using FitnessApp.Services.Data.Contracts;
 using Microsoft.AspNetCore.Identity;
@@ -6,63 +7,75 @@ using Microsoft.EntityFrameworkCore;
 
 namespace FitnessApp.Web
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-			builder.Services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlServer(connectionString));
-			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-			// За сега ги оставям така за да мога да се логвам по лесно
-			builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-				{
-					options.SignIn.RequireConfirmedAccount = false;
-					options.Password.RequireDigit = false;
-					options.Password.RequireUppercase = false;
-					options.Password.RequireNonAlphanumeric = false;
-				})
-				.AddEntityFrameworkStores<ApplicationDbContext>();
+            // За сега ги оставям така за да мога да се логвам по лесно
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders()
+            .AddDefaultUI();
 
-			builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews();
             // Add Razor Pages
+            builder.Services.AddRazorPages();
             builder.Services.AddScoped<ISpaProcedureService, SpaProcedureService>();
             builder.Services.AddScoped<IFitnessEventService, FitnessEventService>();
-			builder.Services.AddScoped<IClassService, ClassService>();
-			builder.Services.AddScoped<IMembershipTypeService, MembershipTypeService>();
+            builder.Services.AddScoped<IClassService, ClassService>();
+            builder.Services.AddScoped<IMembershipTypeService, MembershipTypeService>();
             builder.Services.AddScoped<IInstructorService, InstructorService>();
 
             WebApplication app = builder.Build();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                RolesSeeder.SeedRoles(services);
+                RolesSeeder.AssignAdminRole(services);
+            }
+
             // Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseMigrationsEndPoint();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthorization();
+            app.UseAuthentication(); // Who am I?
+            app.UseAuthorization(); // What can I do?
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
-			app.MapRazorPages();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
 
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
