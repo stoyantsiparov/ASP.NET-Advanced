7b561f6a74116852ea26fde652181bf6e4ec0721
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-06 22:19:56 +0200

    Add Admin HomeController and Dashboard view

---
+++ CSharpWeb-FitnessApp/FitnessApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using FitnessApp.Web.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FitnessApp.Common.ApplicationsConstants;
+
+namespace FitnessApp.Web.Areas.Admin.Controllers;
+
+[Area(AdminRole)]
+[Authorize(Roles = AdminRole)]
+public class HomeController : BaseController
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
