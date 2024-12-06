adadc1f10b7d7bedf4fe0407d171eebebbb549e6
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-06 22:17:26 +0200

    Add AllUsersViewModel class for user management

---
+++ CSharpWeb-FitnessApp/FitnessApp.Web.ViewModels/Admin/UserManagement/AllUsersViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FitnessApp.Web.ViewModels.Admin.UserManagement;
+
+public class AllUsersViewModel
+{
+    public string Id { get; set; } = null!;
+    public string? Email { get; set; }
+    public IEnumerable<string> Roles { get; set; } = null!;
+}
