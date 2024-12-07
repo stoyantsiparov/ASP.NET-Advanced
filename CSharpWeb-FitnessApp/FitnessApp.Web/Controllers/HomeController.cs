0087c542574baeb6b0164cc6a2a5bc94b6c7550d
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-07 11:31:52 +0200

    Refactor HomeController and add error handling

--- CSharpWeb-FitnessApp/FitnessApp.Web/Controllers/HomeController.cs
+++ CSharpWeb-FitnessApp/FitnessApp.Web/Controllers/HomeController.cs
@@ -1,17 +1,34 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FitnessApp.Web.Controllers;
 
-public class HomeController : BaseController
+public class HomeController : Controller
 {
-    [AllowAnonymous]
-    public IActionResult Index()
-    {
-        ViewData["Title"] = "Achieve Your Fitness Goals";
-        ViewData["Message"] = "Join our fitness and wellness community today!";
-        ViewData["CTAButtonText"] = "Get Started";
-        ViewData["Description"] = "Explore our fitness classes and rejuvenating spa services. Whether you’re looking to get fit or relax, we’ve got you covered.";
-        return View();
-    }
+	public IActionResult Index()
+	{
+		ViewData["Title"] = "Achieve Your Fitness Goals";
+		ViewData["Message"] = "Join our fitness and wellness community today!";
+		ViewData["CTAButtonText"] = "Get Started";
+		ViewData["Description"] = "Explore our fitness classes and rejuvenating spa services. Whether you’re looking to get fit or relax, we’ve got you covered.";
+		return View();
+	}
+
+	public IActionResult Error(int? statusCode = null)
+	{
+		if (!statusCode.HasValue)
+		{
+			return View();
+		}
+
+		if (statusCode == 404)
+		{
+			return View("Error404");
+		}
+		else if (statusCode == 401 || statusCode == 403)
+		{
+			return View("Error403");
+		}
+
+		return View("Error500");
+	}
 }
