4d4d29606eafba601e8d11df4d96b725f55ce8d1
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-07 20:37:31 +0200

    Add AppDbContext to UserService and enhance deletion

--- CSharpWeb-FitnessApp/FitnessApp.Services.Data/UserService.cs
+++ CSharpWeb-FitnessApp/FitnessApp.Services.Data/UserService.cs
@@ -1,4 +1,5 @@
-﻿using FitnessApp.Services.Data.Contracts;
+﻿using FitnessApp.Data;
+using FitnessApp.Services.Data.Contracts;
 using FitnessApp.Web.ViewModels.Admin.UserManagement;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -9,11 +10,13 @@ public class UserService : IUserService
 {
     private readonly UserManager<IdentityUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly ApplicationDbContext _context;
 
-    public UserService(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+    public UserService(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
     {
         _userManager = userManager;
         _roleManager = roleManager;
+        _context = context;
     }
 
     /// <summary>
@@ -46,10 +49,10 @@ public class UserService : IUserService
     /// </summary>
     public async Task<bool> UserExistsByIdAsync(string userId)
     {
-       var user = await _userManager
-           .FindByIdAsync(userId);
+        var user = await _userManager
+            .FindByIdAsync(userId);
 
-       return user != null;
+        return user != null;
     }
 
     /// <summary>
@@ -73,7 +76,7 @@ public class UserService : IUserService
 
         if (!alreadyInRole)
         {
-           var result = await _userManager
+            var result = await _userManager
                 .AddToRoleAsync(user, role);
 
             if (!result.Succeeded)
@@ -131,8 +134,26 @@ public class UserService : IUserService
             return false;
         }
 
-        var result = await _userManager
-            .DeleteAsync(user);
+        // Delete related records in ClassesRegistrations
+        var relatedRegistrations = _context.ClassesRegistrations
+            .Where(cr => cr.MemberId == userId);
+        _context.ClassesRegistrations.RemoveRange(relatedRegistrations);
+
+        // Delete related records in SpaRegistrations
+        var relatedSpaRegistrations = _context.SpaRegistrations
+            .Where(sr => sr.MemberId == userId);
+        _context.SpaRegistrations.RemoveRange(relatedSpaRegistrations);
+
+        // Delete related records in EventRegistrations
+        var relatedEventRegistrations = _context.EventRegistrations
+            .Where(er => er.MemberId == userId);
+        _context.EventRegistrations.RemoveRange(relatedEventRegistrations);
+
+        // Save changes to delete related records
+        await _context.SaveChangesAsync();
+
+        // Delete the user
+        var result = await _userManager.DeleteAsync(user);
 
         if (!result.Succeeded)
         {
