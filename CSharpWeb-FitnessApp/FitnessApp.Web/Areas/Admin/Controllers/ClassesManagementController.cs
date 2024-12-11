d0b9a5dec8e3508457cec48bbfded67f88514dc4
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-11 15:30:12 +0200

    Added error and success validation logic and messages

--- CSharpWeb-FitnessApp/FitnessApp.Web/Areas/Admin/Controllers/ClassesManagementController.cs
+++ CSharpWeb-FitnessApp/FitnessApp.Web/Areas/Admin/Controllers/ClassesManagementController.cs
@@ -4,104 +4,113 @@ using FitnessApp.Web.ViewModels.ClassViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using static FitnessApp.Common.ApplicationsConstants;
+using static FitnessApp.Common.SuccessfulValidationMessages.Class;
+using static FitnessApp.Common.ErrorMessages.Class;
 
 namespace FitnessApp.Web.Areas.Admin.Controllers
 {
-	[Area(AdminRole)]
-	[Authorize(Roles = AdminRole)]
-	public class ClassesManagementController : BaseController
-	{
-		private readonly IClassService _classService;
+    [Area(AdminRole)]
+    [Authorize(Roles = AdminRole)]
+    public class ClassesManagementController : BaseController
+    {
+        private readonly IClassService _classService;
 
-		public ClassesManagementController(IClassService classService)
-		{
-			_classService = classService;
-		}
+        public ClassesManagementController(IClassService classService)
+        {
+            _classService = classService;
+        }
 
-		public async Task<IActionResult> Index()
-		{
-			var classes = await _classService.GetAllClassesAsync();
+        public async Task<IActionResult> Index()
+        {
+            var classes = await _classService.GetAllClassesAsync();
 
-			return View(classes);
-		}
+            return View(classes);
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> Add()
-		{
-			var model = await _classService.GetClassForAddAsync();
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = await _classService.GetClassForAddAsync();
 
-			return View(model);
-		}
+            return View(model);
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Add(AddClassViewModel model)
-		{
-			if (ModelState.IsValid == false)
-			{
-				var instructors = await _classService.GetClassForAddAsync();
-				model.Instructors = instructors.Instructors;
-				return View(model);
-			}
+        [HttpPost]
+        public async Task<IActionResult> Add(AddClassViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = InvalidData;
+                var instructors = await _classService.GetClassForAddAsync();
+                model.Instructors = instructors.Instructors;
+                return View(model);
+            }
 
-			var userId = GetUserId();
-			await _classService.AddClassAsync(model, userId);
+            var userId = GetUserId();
+            await _classService.AddClassAsync(model, userId);
 
-			return RedirectToAction(nameof(Index));
-		}
+            TempData["SuccessMessage"] = ClassAddedSuccessfully;
+            return RedirectToAction(nameof(Index));
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> Edit(int id)
-		{
-			var model = await _classService.GetClassByIdAsync(id);
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await _classService.GetClassByIdAsync(id);
 
-			if (model != null)
-			{
-				var instructors = await _classService.GetClassForAddAsync();
-				model.Instructors = instructors.Instructors;
-				return View(model);
-			}
+            if (model != null)
+            {
+                var instructors = await _classService.GetClassForAddAsync();
+                model.Instructors = instructors.Instructors;
+                return View(model);
+            }
 
-			return RedirectToAction(nameof(Index));
-		}
+            TempData["ErrorMessage"] = ClassNotFound;
+            return RedirectToAction(nameof(Index));
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Edit(ClassesViewModel model)
-		{
-			if (ModelState.IsValid == false)
-			{
-				var instructors = await _classService.GetClassForAddAsync();
-				model.Instructors = instructors.Instructors;
-				return View(model);
-			}
+        [HttpPost]
+        public async Task<IActionResult> Edit(ClassesViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMessage"] = InvalidData;
+                var instructors = await _classService.GetClassForAddAsync();
+                model.Instructors = instructors.Instructors;
+                return View(model);
+            }
 
-			var userId = GetUserId();
+            var userId = GetUserId();
 
-			await _classService.EditClassAsync(model, userId);
+            await _classService.EditClassAsync(model, userId);
 
-			return RedirectToAction(nameof(Index));
-		}
+            TempData["SuccessMessage"] = ClassUpdatedSuccessfully;
+            return RedirectToAction(nameof(Index));
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> Delete(int id)
-		{
-			var model = await _classService.GetClassForDeleteAsync(id);
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var model = await _classService.GetClassForDeleteAsync(id);
 
-			if (model != null)
-			{
-				return View(model);
-			}
+            if (model != null)
+            {
+                return View(model);
+            }
 
-			return RedirectToAction(nameof(Index));
-		}
+            TempData["ErrorMessage"] = ClassNotFound;
+            return RedirectToAction(nameof(Index));
+        }
 
-		[HttpPost]
-		public async Task<IActionResult> Delete(DeleteClassViewModel model)
-		{
-			var userId = GetUserId();
+        [HttpPost]
+        public async Task<IActionResult> Delete(DeleteClassViewModel model)
+        {
+            var userId = GetUserId();
 
-			await _classService.DeleteClassAsync(model.Id, userId);
+            await _classService.DeleteClassAsync(model.Id, userId);
 
-			return RedirectToAction(nameof(Index));
-		}
-	}
+            TempData["SuccessMessage"] = ClassDeletedSuccessfully;
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
