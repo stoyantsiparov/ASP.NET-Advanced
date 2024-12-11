62d79e5e24d33b315ff56b32ceb43c5a09243ecc
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-12-11 15:10:24 +0200

    Added error and success validation logic and messages

--- CSharpWeb-FitnessApp/FitnessApp.Web/Areas/Admin/Controllers/MembershipTypesManagementController.cs
+++ CSharpWeb-FitnessApp/FitnessApp.Web/Areas/Admin/Controllers/MembershipTypesManagementController.cs
@@ -4,6 +4,8 @@ using FitnessApp.Web.ViewModels.MembershipTypeViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using static FitnessApp.Common.ApplicationsConstants;
+using static FitnessApp.Common.SuccessfulValidationMessages.MembershipType;
+using static FitnessApp.Common.ErrorMessages.MembershipType;
 
 namespace FitnessApp.Web.Areas.Admin.Controllers;
 
@@ -11,90 +13,97 @@ namespace FitnessApp.Web.Areas.Admin.Controllers;
 [Authorize(Roles = AdminRole)]
 public class MembershipTypesManagementController : BaseController
 {
-	private readonly IMembershipTypeService _membershipTypeService;
+    private readonly IMembershipTypeService _membershipTypeService;
 
-	public MembershipTypesManagementController(IMembershipTypeService membershipTypeService)
-	{
-		_membershipTypeService = membershipTypeService;
-	}
+    public MembershipTypesManagementController(IMembershipTypeService membershipTypeService)
+    {
+        _membershipTypeService = membershipTypeService;
+    }
 
-	public async Task<IActionResult> Index()
-	{
-		var membershipTypes = await _membershipTypeService.GetAllMembershipTypesAsync();
+    public async Task<IActionResult> Index()
+    {
+        var membershipTypes = await _membershipTypeService.GetAllMembershipTypesAsync();
 
-		return View(membershipTypes);
-	}
+        return View(membershipTypes);
+    }
 
-	[HttpGet]
-	public async Task<IActionResult> Add()
-	{
-		var model = await _membershipTypeService.GetMembershipTypeForAddAsync();
+    [HttpGet]
+    public async Task<IActionResult> Add()
+    {
+        var model = await _membershipTypeService.GetMembershipTypeForAddAsync();
 
-		return View(model);
-	}
+        return View(model);
+    }
 
-	[HttpPost]
-	public async Task<IActionResult> Add(AddMembershipTypeViewModel model)
-	{
-		if (ModelState.IsValid == false)
-		{
-			return View(model);
-		}
+    [HttpPost]
+    public async Task<IActionResult> Add(AddMembershipTypeViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = InvalidData;
+            return View(model);
+        }
 
-		var userId = GetUserId();
-		await _membershipTypeService.AddMembershipTypeAsync(model, userId);
+        var userId = GetUserId();
+        await _membershipTypeService.AddMembershipTypeAsync(model, userId);
 
-		return RedirectToAction(nameof(Index));
-	}
+        TempData["SuccessMessage"] = MembershipTypeAddedSuccessfully;
+        return RedirectToAction(nameof(Index));
+    }
 
-	[HttpGet]
-	public async Task<IActionResult> Edit(int id)
-	{
-		var model = await _membershipTypeService.GetMembershipTypeByIdAsync(id);
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var model = await _membershipTypeService.GetMembershipTypeByIdAsync(id);
 
-		if (model != null)
-		{
-			return View(model);
-		}
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = MembershipNotFound;
+            return RedirectToAction(nameof(Index));
+        }
 
-		return RedirectToAction(nameof(Index));
-	}
+        return View(model);
+    }
 
-	[HttpPost]
-	public async Task<IActionResult> Edit(MembershipTypeViewModel model)
-	{
-		if (ModelState.IsValid == false)
-		{
-			return View(model);
-		}
+    [HttpPost]
+    public async Task<IActionResult> Edit(MembershipTypeViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = InvalidData;
+            return View(model);
+        }
 
-		var userId = GetUserId();
+        var userId = GetUserId();
 
-		await _membershipTypeService.EditMembershipTypeAsync(model, userId);
+        await _membershipTypeService.EditMembershipTypeAsync(model, userId);
 
-		return RedirectToAction(nameof(Index));
-	}
+        TempData["SuccessMessage"] = MembershipTypeUpdatedSuccessfully;
+        return RedirectToAction(nameof(Index));
+    }
 
-	[HttpGet]
-	public async Task<IActionResult> Delete(int id)
-	{
-		var model = await _membershipTypeService.GetMembershipTypeForDeleteAsync(id);
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var model = await _membershipTypeService.GetMembershipTypeForDeleteAsync(id);
 
-		if (model != null)
-		{
-			return View(model);
-		}
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = MembershipNotFound;
+            return RedirectToAction(nameof(Index));
+        }
 
-		return RedirectToAction(nameof(Index));
-	}
+        return View(model);
+    }
 
-	[HttpPost]
-	public async Task<IActionResult> Delete(DeleteMembershipTypeViewModel model)
-	{
-		var userId = GetUserId();
+    [HttpPost]
+    public async Task<IActionResult> Delete(DeleteMembershipTypeViewModel model)
+    {
+        var userId = GetUserId();
 
-		await _membershipTypeService.DeleteMembershipTypeAsync(model.Id, userId);
+        await _membershipTypeService.DeleteMembershipTypeAsync(model.Id, userId);
 
-		return RedirectToAction(nameof(Index));
-	}
+        TempData["SuccessMessage"] = MembershipTypeDeletedSuccessfully;
+        return RedirectToAction(nameof(Index));
+    }
 }
